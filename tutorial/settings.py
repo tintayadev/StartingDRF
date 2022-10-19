494dc15e519a8045640271f232d9dd928fc02a77
Paulo Tintaya <pau.tintaya.code@gamil.com>
2022-10-19 10:29:45 -0400

    SessionAuthentication added rest_framework.authentication

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -128,6 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'PAGE_SIZE': 10
 }
